79ec6cfdbd628a62f5e6fb78d9767d8a4e250f5f
miia <miia@com.tw>
2019-11-12 16:15:44 +0800

    third commit

--- app/src/main/java/com/fju/water3/MainActivity.java
+++ app/src/main/java/com/fju/water3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fju.water3;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +15,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edMonth;
     private EditText edNext;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,79 +39,67 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        reset();
+                    }
+                };
+                if(!TextUtils.isEmpty(edMonth.getText().toString())){
+                    float num = Float.parseFloat(edMonth.getText().toString());
+                    float fee = 0;
+                    if(num>=1&&num<=10){
+                        fee = num*7.35f;
+                    }else if(num>=11&&num<=30){
+                        fee = num*9.45f-21;
+                    }else if(num>=31&&num<=50){
+                        fee = num*11.55f-84;
+                    }else if(num>51){
+                        fee = num*120.75f-110.25f;
+                    }
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    startActivity(intent);
+//             new AlertDialog.Builder(MainActivity.this)
+//                     .setTitle("每月抄表費用")
+//                     .setMessage("費用:"+fee)
+//                     .setPositiveButton("Ok",listener)
+//                     .show();
+                }
+                if(!TextUtils.isEmpty(edNext.getText().toString())) {
+                    float number = Float.parseFloat(edNext.getText().toString());
+                    float fee = 0;
+                    if (number >= 1 && number <= 20) {
+                        fee = number*7.35f;
+                    } else if (number >= 21 && number <= 60) {
+                        fee = number*9.45f-21;
+                    } else if (number >= 61 && number <= 100) {
+                        fee = number*11.55f-168;
+                    } else if (number > 101) {
+                        fee = number*12.075f-220.5f;
+                    }
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    startActivity(intent);
+//            new AlertDialog.Builder(MainActivity.this)
+//                    .setTitle("隔月抄表費用")
+//                    .setMessage("費用:" + fee)
+//                    .setPositiveButton("Ok", listener)
+//                    .show();
+                }
+            }
+        });
     }
-    public void reset(){
+    public void fee(){
+
+    }
+    public void reset() {
         String message = "";
         edMonth.setText(message);
         edNext.setText(message);
-    }
-
-    public  void Button(View view){
-        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            reset();
-        }
-    };
-        if(!TextUtils.isEmpty(edMonth.getText().toString())){
-            float num = Float.parseFloat(edMonth.getText().toString());
-            if(num>=1&&num<=10){
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("每月抄表費用")
-                        .setMessage("費用:"+num*7.35)
-                        .setPositiveButton("Ok",listener)
-                        .show();
-            }else if(num>=11&&num<=30){
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("每月抄表費用")
-                        .setMessage("費用:"+(num*9.45-21))
-                        .setPositiveButton("Ok",listener)
-                        .show();
-
-            }else if(num>=31&&num<=50){
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("每月抄表費用")
-                        .setMessage("費用:"+(num*11.55-84))
-                        .setPositiveButton("Ok",listener)
-                        .show();
-            }else if(num>51){
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("每月抄表費用")
-                        .setMessage("費用:"+(num*12.075-110.25))
-                        .setPositiveButton("Ok",listener)
-                        .show();
-            }
-
-            }
-        if(!TextUtils.isEmpty(edNext.getText().toString())) {
-            float number = Float.parseFloat(edNext.getText().toString());
-            if (number >= 1 && number <= 20) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("隔月抄表費用")
-                        .setMessage("費用:" + number * 7.35)
-                        .setPositiveButton("Ok", listener)
-                        .show();
-            } else if (number >= 21 && number <= 60) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("隔月抄表費用")
-                        .setMessage("費用:" + (number * 9.45-42))
-                        .setPositiveButton("Ok", listener)
-                        .show();
-
-            } else if (number >= 61 && number <= 100) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("隔月抄表費用")
-                        .setMessage("費用:" + (number * 11.55-168))
-                        .setPositiveButton("Ok", listener)
-                        .show();
-            } else if (number > 101) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("隔月抄表費用")
-                        .setMessage("費用:" +(number * 12.075-220.5))
-                        .setPositiveButton("Ok", listener)
-                        .show();
-            }
-        }
     }
 
     @Override
