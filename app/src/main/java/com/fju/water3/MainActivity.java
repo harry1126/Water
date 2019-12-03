2c6a19f771083eff529bd4d91dc7834214f266a5
miia <miia@com.tw>
2019-12-03 16:29:48 +0800

    12/3 下拉選單 notepad++ 生命週期

--- app/src/main/java/com/fju/water3/MainActivity.java
+++ app/src/main/java/com/fju/water3/MainActivity.java
@@ -12,22 +12,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName() ;
     boolean isNext = false;
     private EditText edMonth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate");
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         edMonth = findViewById(R.id.month);
@@ -46,6 +51,19 @@ public class MainActivity extends AppCompatActivity {
                 isNext = isChecked;
                 TextView text = findViewById(R.id.type);
                 text.setText(isNext ?getString(R.string.every_other_month):getString(R.string.monthly));
+            }
+        });
+        //spinner
+        Spinner cities = findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG,getResources().getStringArray(R.array.cities)[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
         Button button = findViewById(R.id.button);
@@ -121,6 +139,41 @@ public class MainActivity extends AppCompatActivity {
     public void reset() {
         String message = "";
         edMonth.setText(message);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onstart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
     }
 
     @Override
